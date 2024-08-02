21b84183b68094c00bebc12f94105ddafb306421
qaws7791 <qaws7791@gmail.com>
2024-08-02 16:19:55 +0900

    remove: ERD

--- app/erd/page.tsx
+++
@@ -1,9 +0,0 @@
-import ErdDiagram from "@/features/erd/components/erd-diagram";
-
-export default function Page() {
-  return (
-    <div className="min-h-screen">
-      <ErdDiagram />
-    </div>
-  );
-}
