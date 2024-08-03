f29dfbabfacbfd0fdb3ccd07c17bd8173887afb3
qaws7791 <qaws7791@gmail.com>
2024-08-03 23:50:44 +0900

    feat: RecommendationWithAuthor 타입 추가

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+import { RecommendationDocument } from "@/db/models/recommendation";
+import { UserDocument } from "@/db/models/user";
 import { SVGProps } from "react";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
@@ -17,4 +19,8 @@ export type Recommendation = {
     coffeeDescription: string;
     keywords: string[];
   }[];
+};
+
+export type RecommendationWithAuthor = RecommendationDocument & {
+  author: UserDocument;
 };
