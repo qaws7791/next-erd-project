061fbf37bbfff344a6050e97c2a6abb31e6b7525
qaws7791 <qaws7791@gmail.com>
2024-08-04 16:27:15 +0900

    feat: landing 페이지로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,28 @@
-import GoogleLoginButton from "@/features/auth/components/google-login-button";
-import LogoutButton from "@/features/auth/components/logout-button";
-import { title } from "@/components/primitives";
+import { redirect } from "next/navigation";
 import { getUser } from "@/lib/user";
-import { Avatar } from "@nextui-org/avatar";
-import ChatForm from "@/app/chat-form";
-import RecommendationList from "@/app/recommendation-list";
-import { Suspense } from "react";
-export default async function Home() {
+import { subtitle, title } from "@/components/primitives";
+import { siteConfig } from "@/config/site";
+export default async function LandingPage() {
   const user = await getUser();
+  if (user) {
+    redirect("/app");
+  }
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 max-w-screen-md mx-auto ">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Login &nbsp;</h1>
-        <br />
-        <h1 className={title()}>Your Google Account</h1>
-      </div>
-      {user ? (
-        <div className="flex items-center flex-col gap-8 w-full p-4">
-          <h2 className={title({ size: "sm" })}>{user.email}</h2>
-          <Avatar src={user.photo} className="w-20 h-20" />
-          <LogoutButton />
-          <ChatForm />
-        </div>
-      ) : (
-        <GoogleLoginButton />
-      )}
-      <Suspense fallback={<div>Loading...</div>}>
-        <RecommendationList />
-      </Suspense>
+      <h1 className={title()}>
+        Welcome to
+        <span
+          className={title({
+            color: "violet",
+            size: "lg",
+            fullWidth: true,
+          })}
+        >
+          My Coffee
+        </span>
+      </h1>
+      <p className={subtitle()}>{siteConfig.description}</p>
     </section>
   );
 }
