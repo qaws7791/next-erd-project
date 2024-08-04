be36a0c663ee8eb39bd97333c4773d6b13b54aba
qaws7791 <qaws7791@gmail.com>
2024-08-04 16:22:44 +0900

    feat: redirect 제거

--- db/actions/logout.ts
+++ db/actions/logout.ts
@@ -1,9 +1,7 @@
 "use server";
 
 import { deleteSession } from "@/lib/session";
-import { redirect } from "next/navigation";
 
 export async function logout() {
   deleteSession();
-  redirect("/");
 }
